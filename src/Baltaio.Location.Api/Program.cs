1726fc425eed463111dbfeceabe70d32b8b68e78
gustavojordao <gustavo.jordao@hotmail.com.br>
2023-10-18 00:26:07 -0300

    Adicionada rota de importação de arquivo Excel (.xlsx)

--- src/Baltaio.Location.Api/Program.cs
+++ src/Baltaio.Location.Api/Program.cs
@@ -1,5 +1,8 @@
 using Baltaio.Location.Api.Application.Addresses.Commons;
 using Baltaio.Location.Api.Infrastructure.Addresses;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using SpreadsheetLight;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +29,64 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapPost("import-excel", (IFormFile file) => ExcelHandler.Import(file));
+
 app.MapControllers();
 
 app.Run();
+
+public static class ExcelHandler
+{
+    public static async Task<List<object>> Import(IFormFile file)
+    {
+        if(!file.FileName.ToLower().EndsWith(".xlsx"))
+            return new List<object>();
+
+        SLDocument document = new SLDocument(file.OpenReadStream());
+        document.SelectWorksheet("ESTADOS");
+
+        List<StateDTO> lstState = new();
+
+        for (int i = 2; i < 29; i++)
+        {
+            lstState.Add(
+                new StateDTO
+                (
+                    document.GetCellValueAsInt32($"A{i}"),
+                    document.GetCellValueAsString($"B{i}"),
+                    document.GetCellValueAsString($"C{i}")
+                )
+            );
+
+        }
+
+        document.SelectWorksheet("MUNICIPIOS");
+
+        List<CityDTO> lstCity = new();
+
+        for (int i = 2; i < 5571; i++)
+        {
+            lstCity.Add(
+                new CityDTO
+                (
+                    document.GetCellValueAsInt32($"A{i}"),
+                    document.GetCellValueAsString($"B{i}"),
+                    document.GetCellValueAsString($"C{i}")
+                )
+            );
+
+        }
+
+        var result = new List<object>();
+
+        result.AddRange(lstState);
+        result.AddRange(lstCity.Take(20));   
+
+        return result;
+
+    }
+}
+
+public record StateDTO(int CodigoUF, string SiglaUF, string NomeUF);
+
+public record CityDTO(int CodigoMunicipio, string NomeMunicipio, string CodigoUF);
