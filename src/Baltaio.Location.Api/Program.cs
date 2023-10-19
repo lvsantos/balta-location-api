f9f2d193d34347fb4d3b183fba40a885841f6334
Lucas Valtudes <lucassantosdesenvolvedor@gmail.com>
2023-10-19 06:42:11 -0300

    fix: Resolvendo resolução de depedências das classes de serviço de registro de usuário e de login

--- src/Baltaio.Location.Api/Program.cs
+++ src/Baltaio.Location.Api/Program.cs
@@ -5,17 +5,23 @@ using Baltaio.Location.Api.Application.Users.Abstractions;
 using Baltaio.Location.Api.Infrastructure.Users.Authentication;
 using Baltaio.Location.Api.Application.Users.Login.Abstractions;
 using Baltaio.Location.Api.Application.Users.Login;
+using Baltaio.Location.Api.Application.Users.Register;
+using Baltaio.Location.Api.Application.Users.Register.Abstraction;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 builder.Services.AddScoped<ICityRepository, CityRepository>();
 builder.Services.AddScoped<IAddressRepository, AddressRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection(JwtSettings.SECTION_NAME));
+
+builder.Services.AddScoped<ILoginAppService, LoginAppService>();
+builder.Services.AddScoped<IRegisterUserAppService, RegisterUserAppService>();
 builder.Services.Configure<SaltSettings>(builder.Configuration.GetSection(SaltSettings.SECTION_NAME));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
