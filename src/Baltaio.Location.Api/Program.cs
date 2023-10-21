2a595688f4375cd54a68a317bd65c0938b9b87f4
Lucas Valtudes <lucassantosdesenvolvedor@gmail.com>
2023-10-21 19:41:20 -0300

    feat: Inserção de versionamento de api para todos os endpoints da aplicação

--- src/Baltaio.Location.Api/Program.cs
+++ src/Baltaio.Location.Api/Program.cs
@@ -1,3 +1,4 @@
+using Asp.Versioning;
 using Baltaio.Location.Api.Application.Addresses.Commons;
 using Baltaio.Location.Api.Application.Data.Import.Commons;
 using Baltaio.Location.Api.Application.Data.Import.ImportData;
@@ -11,93 +12,139 @@ using Baltaio.Location.Api.Infrastructure;
 using Baltaio.Location.Api.Infrastructure.Addresses;
 using Baltaio.Location.Api.Infrastructure.Users.Authentication;
 using Baltaio.Location.Api.Infrastructure.Users.Persistance;
+using Baltaio.Location.Api.OpenApi;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-builder.Services.AddScoped<ICityRepository, CityRepository>();
-builder.Services.AddScoped<IStateRepository, StateRepository>();
-builder.Services.AddScoped<IFileRepository, FileRepository>();
-builder.Services.AddScoped<IAddressRepository, AddressRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+{
+    builder.Services.AddControllers();
+    builder.Services.AddApiVersioning(options =>
     {
-        JwtSettings? jwtSettings = builder.Configuration.GetSection(JwtSettings.SECTION_NAME).Get<JwtSettings>();
-        ArgumentNullException.ThrowIfNull(jwtSettings, nameof(jwtSettings));
-
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtSettings.Issuer,
-            ValidAudience = jwtSettings.Audience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
-        };
+        options.ReportApiVersions = true;
+        options.ApiVersionReader = new UrlSegmentApiVersionReader();
+    }).AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
     });
-builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
-builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection(JwtSettings.SECTION_NAME));
 
-builder.Services.AddScoped<ILoginAppService, LoginAppService>();
-builder.Services.AddScoped<IRegisterUserAppService, RegisterUserAppService>();
-builder.Services.Configure<SaltSettings>(builder.Configuration.GetSection(SaltSettings.SECTION_NAME));
+    builder.Services.AddScoped<ICityRepository, CityRepository>();
+    builder.Services.AddScoped<IStateRepository, StateRepository>();
+    builder.Services.AddScoped<IFileRepository, FileRepository>();
+    builder.Services.AddScoped<IAddressRepository, AddressRepository>();
+    builder.Services.AddScoped<IUserRepository, UserRepository>();
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            JwtSettings? jwtSettings = builder.Configuration.GetSection(JwtSettings.SECTION_NAME).Get<JwtSettings>();
+            ArgumentNullException.ThrowIfNull(jwtSettings, nameof(jwtSettings));
 
-builder.Services.AddScoped<IImportDataAppService, ImportDataAppService>();
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.Secret))
+            };
+        });
+    builder.Services.AddScoped<IJwtGenerator, JwtGenerator>();
+    builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection(JwtSettings.SECTION_NAME));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Services.AddScoped<ILoginAppService, LoginAppService>();
+    builder.Services.AddScoped<IRegisterUserAppService, RegisterUserAppService>();
+    builder.Services.Configure<SaltSettings>(builder.Configuration.GetSection(SaltSettings.SECTION_NAME));
+
+    builder.Services.AddScoped<IImportDataAppService, ImportDataAppService>();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+    builder.Services.AddSwaggerGen(option => option.OperationFilter<SwaggerDefaultValues>());
+}
 
 var app = builder.Build();
+{
+    var versionSet = app.NewApiVersionSet()
+        .HasApiVersion(new ApiVersion(majorVersion: 1, minorVersion: 0))
+        .HasApiVersion(new ApiVersion(majorVersion: 1, minorVersion: 1))
+        .HasApiVersion(new ApiVersion(majorVersion: 2, minorVersion: 0))
+        .ReportApiVersions()
+        .Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+    #region Routes
+
+    app.MapGet("is-alive", () => Results.Ok());
+
+    app.MapPost("api/v{version:apiVersion}/auth/register", ([FromBody] RegisterUserRequest request) => RegisterAsync(request))
+        .WithApiVersionSet(versionSet)
+        .MapToApiVersion(new ApiVersion(majorVersion: 1, minorVersion: 0));
+    app.MapPost("api/v{version:apiVersion}/auth/login", ([FromBody] LoginRequest request) => LoginAsync(request))
+        .WithApiVersionSet(versionSet)
+        .MapToApiVersion(new ApiVersion(majorVersion: 1, minorVersion: 0));
+
+    app.MapPost("api/v{version:apiVersion}/locations", () => Results.Ok())
+        .RequireAuthorization()
+        .WithApiVersionSet(versionSet)
+        .MapToApiVersion(new ApiVersion(majorVersion: 1, minorVersion: 0));
+    app.MapGet("api/v{version:apiVersion}/locations/{id}", () => Results.Ok())
+        .RequireAuthorization()
+        .WithApiVersionSet(versionSet)
+        .MapToApiVersion(new ApiVersion(majorVersion: 1, minorVersion: 0));
+    app.MapPut("api/v{version:apiVersion}/locations/{id}", () => Results.Ok())
+        .RequireAuthorization()
+        .WithApiVersionSet(versionSet)
+        .MapToApiVersion(new ApiVersion(majorVersion: 1, minorVersion: 0));
+    app.MapDelete("api/v{version:apiVersion}/locations/{id}", () => Results.Ok())
+        .RequireAuthorization()
+        .WithApiVersionSet(versionSet)
+        .MapToApiVersion(new ApiVersion(majorVersion: 1, minorVersion: 0));
+    app.MapGet("api/v{version:apiVersion}/locations", () => Results.Ok())
+        .RequireAuthorization()
+        .WithApiVersionSet(versionSet)
+        .MapToApiVersion(new ApiVersion(majorVersion: 1, minorVersion: 0));
+    app.MapPost("api/v{version:apiVersion}/locations/import-data", (IFormFile file) => ImportData(file))
+        .RequireAuthorization()
+        .WithApiVersionSet(versionSet)
+        .MapToApiVersion(new ApiVersion(majorVersion: 1, minorVersion: 0));
+
+    #endregion
+
+    // Configure the HTTP request pipeline.
+    //if (app.Environment.IsDevelopment())
+    //{
     app.UseSwagger();
-    app.UseSwaggerUI();
-//}
+    app.UseSwaggerUI(
+        options =>
+        {
+            var descriptions = app.DescribeApiVersions();
 
-app.UseHttpsRedirection();
+            // build a swagger endpoint for each discovered API version
+            foreach (var description in descriptions)
+            {
+                var url = $"/swagger/{description.GroupName}/swagger.json";
+                var name = description.GroupName.ToUpperInvariant();
+                options.SwaggerEndpoint(url, name);
+            }
+        });
+    //}
 
-app.UseAuthentication();
-app.UseAuthorization();
+    app.UseHttpsRedirection();
 
-#region Routes
+    app.UseAuthentication();
+    app.UseAuthorization();
 
-app.MapGet("is-alive", () => Results.Ok());
+    app.MapControllers();
 
-app.MapPost("api/auth/register", ([FromBody] RegisterUserRequest request) => RegisterAsync(request));
-app.MapPost("api/auth/login", ([FromBody] LoginRequest request) => LoginAsync(request));
-
-app.MapPost("api/locations", () => Results.Ok())
-    .RequireAuthorization();
-app.MapGet("api/locations/{id}", () => Results.Ok())
-    .RequireAuthorization();
-app.MapPut("api/locations/{id}", () => Results.Ok())
-    .RequireAuthorization();
-app.MapDelete("api/locations/{id}", () => Results.Ok())
-    .RequireAuthorization();
-app.MapGet("api/locations", () => Results.Ok())
-    .RequireAuthorization();
-app.MapPost("api/locations/import-data", (IFormFile file) => ImportData(file))
-    .RequireAuthorization();
-
-#endregion
-
-app.MapControllers();
-
-app.Run();
+    app.Run();
+}
 
 async Task<IResult> RegisterAsync(RegisterUserRequest request)
 {
