1881b90923ff1c4ad429eeabd65993cf94eba20b
Lucas Valtudes <lucassantosdesenvolvedor@gmail.com>
2023-10-21 23:15:17 -0300

    refactor: Remoção de dicionário em memória para utilização do banco de dados no fluxo de registro e login de usuários

--- src/Baltaio.Location.Api/Application/Users/Abstractions/IUserRepository.cs
+++ src/Baltaio.Location.Api/Application/Users/Abstractions/IUserRepository.cs
@@ -4,7 +4,7 @@ namespace Baltaio.Location.Api.Application.Users.Abstractions;
 
 public interface IUserRepository
 {
-    Task<bool> ExistsAsync(string email);
-    Task<User?> LoginAsync(User user);
-    Task SaveAsync(User user);
+    Task<bool> ExistsAsync(string email, CancellationToken cancellationToken = default);
+    Task<User?> LoginAsync(User userToSearch, CancellationToken cancellationToken = default);
+    Task SaveAsync(User user, CancellationToken cancellationToken = default);
 }
