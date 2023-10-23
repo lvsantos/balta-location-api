526df04627b3744908c2f9ed8bfa84a3cc679c5c
gustavojordao <gustavo.jordao@hotmail.com.br>
2023-10-22 21:11:24 -0300

    Adicionada tratativa de cidades e estados duplicados em importação via excel

--- src/Baltaio.Location.Api/Application/Data/Import/ImportData/ImportDataAppService.cs
+++ src/Baltaio.Location.Api/Application/Data/Import/ImportData/ImportDataAppService.cs
@@ -1,8 +1,6 @@
 ﻿using Baltaio.Location.Api.Application.Addresses.Commons;
 using Baltaio.Location.Api.Application.Data.Import.Commons;
 using Baltaio.Location.Api.Domain;
-using Baltaio.Location.Api.Infrastructure;
-using Microsoft.AspNetCore.Components;
 
 namespace Baltaio.Location.Api.Application.Data.Import.ImportData
 {
@@ -22,12 +20,34 @@ namespace Baltaio.Location.Api.Application.Data.Import.ImportData
         public async Task<ImportDataOutput> Execute(Stream source)
         {
             var states = _fileRepository.GetStates(source);
+            states = await GetOnlyNotFilledInStates(states);
             await _stateRepository.AddAllAsync(states);
 
             var cities = _fileRepository.GetCities(source, states);
+            cities = await GetOnlyNotFilledInCities(cities);
             await _cityRepository.AddAllAsync(cities);
 
             return new ImportDataOutput(states.Count, cities.Count);
+        }
+
+        private async Task<List<State>> GetOnlyNotFilledInStates(List<State> states)
+        {
+            var alreadyFilledInStates = await _stateRepository.GetAllAsync();
+
+            return states.Where((state) =>
+                        !alreadyFilledInStates.Exists(
+                            (alreadyFilledInState) => alreadyFilledInState.Code == state.Code
+                        )).ToList();
+        }
+
+        private async Task<List<City>> GetOnlyNotFilledInCities(List<City> cities)
+        {
+            var alreadyFilledInCities = await _cityRepository.GetByStateOrCityAsync("", "");
+
+            return cities.Where((city) =>
+                        !alreadyFilledInCities.Exists(
+                            (alreadyFilledInCity) => alreadyFilledInCity.Code == city.Code
+                        )).ToList();
         }
 
     }
